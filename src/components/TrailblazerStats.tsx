3f7506e92210c13c07338244862ac10cb0fbc99f
Sodynoizz_TH <chonlasitapilardmongkol@gmail.com>
2025-06-19 16:20:53 +0700

    fix: import error

--- src/components/TrailblazerStats.tsx
+++ src/components/TrailblazerStats.tsx
@@ -2,7 +2,6 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Battery, Clock, Star, Trophy, Award } from "lucide-react";
 
-import { getElementColor, getRarityColor } from "@/app/utils/getColor";
 import { TrailblazerStatsProps } from "@/app/types/starrail";
 
 const TrailblazerStats = (
